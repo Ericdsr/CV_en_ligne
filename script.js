7ffa7d2b27066edcf0251e423611ed01bf5d9e24
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-04-23 18:01:26 +0200

    Manque modif sur l'écriture darkmode

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const section1 = document.querySelector('#first');
 const section2 = document.querySelector('#second');
 const section3 = document.querySelector('#third');
 const section4 = document.querySelector('#four');
+const footer1 = document.querySelector('#footer');
 
 toggleThemeBtn.addEventListener('click', function() {
   toggleThemeBtn.classList.toggle('dark');
   section1.classList.toggle('dark');
   section3.classList.toggle('dark');
-  section4.classList.toggle('dark');
-
+  footer1.classList.toggle('dark');
 });
