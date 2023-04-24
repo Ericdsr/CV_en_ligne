03b17761569ced43f6c3a8aade8a50e404520400
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-04-24 10:28:46 +0200

    tablette done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const section1 = document.querySelector('#first');
 const section2 = document.querySelector('#second');
 const section3 = document.querySelector('#third');
 const section4 = document.querySelector('#four');
-const footer1 = document.querySelector('#footer');
+const footer1 = document.querySelector('footer');
 
 toggleThemeBtn.addEventListener('click', function() {
   toggleThemeBtn.classList.toggle('dark');
