8776f5804a312ab5e5880039e6dd62dc90093586
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-04-28 13:59:55 +0200

    Smooth criminal

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
 const section1 = document.querySelector('#first');
-const section2 = document.querySelector('#second');
 const section3 = document.querySelector('#third');
-const section4 = document.querySelector('#four');
 const footer1 = document.querySelector('footer');
 const content = document.querySelector('#content');
+const rese = document.querySelector('#reseaux');
 
 toggleThemeBtn.addEventListener('click', function() {
   toggleThemeBtn.classList.toggle('dark');
   section3.classList.toggle('dark');
   footer1.classList.toggle('dark');
   content.classList.toggle('dark');
+  rese.classList.toggle('dark');
+
 });
