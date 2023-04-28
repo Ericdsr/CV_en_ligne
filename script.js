7f700e2f92ada5bf00f1af6f7464de92e58831df
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-04-28 12:13:38 +0200

    peux etre navbar diff

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const section2 = document.querySelector('#second');
 const section3 = document.querySelector('#third');
 const section4 = document.querySelector('#four');
 const footer1 = document.querySelector('footer');
+const content = document.querySelector('#content');
 
 toggleThemeBtn.addEventListener('click', function() {
   toggleThemeBtn.classList.toggle('dark');
-  section1.classList.toggle('dark');
   section3.classList.toggle('dark');
   footer1.classList.toggle('dark');
+  content.classList.toggle('dark');
 });
